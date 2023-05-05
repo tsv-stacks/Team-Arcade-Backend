07ac1d1c7e5549922b087cfd7d4be7492489581c
Matthew Giles <matty92g@hotmail.co.uk>
2023-05-05 20:03:28 +0100

    add -A

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const uri = process.env.DATABASE_URI;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connected');
+});
+
+const scoreBoardRouter = require('./routes/scoreBoard');
+const usersRouter = require('./routes/users');
+
+app.use('/scoreboard', scoreBoardRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server on port: ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('NODE API');
+});
